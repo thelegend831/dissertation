5e9faabe242e73b8a7ef93f7e28a749130b65ffb
aj <andrewjpack@gmail.com>
2020-04-15 19:10:23 +0100

    started frame throttling, flattened XR apps, removed snippets

--- snippets/frame.cs
+++ snippets/frame.cs
@@ -1,21 +1,8 @@
-public struct Frame
+public class Frame
 {
     public List<Single> Vertices;
     public List<byte> RGB;
     public List<Body> Bodies;
-    public int ClientID;
+    public int SourceID;
     public List<AffineTransform> CameraPoses;
-
-    public Frame(List<Single> vertsin, 
-                    List<byte> rgbin, 
-                    List<Body> bodiesin, 
-                    int clientID, 
-                    List<AffineTransform> cameraPosesin)
-    {
-        Vertices = vertsin;
-        RGB = rgbin;
-        Bodies = bodiesin;
-        ClientID = clientID;
-        CameraPoses = cameraPosesin;
-    }
 }
